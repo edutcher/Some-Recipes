1ebcb81318d01058b706b9ff984b8b7aae9758ec
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-04-20 00:41:21 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+  const path = require('path');
+  const express = require("express");
+  const logger = require("morgan");
+  const mongoose = require("mongoose");
+  //const routes = require('./routes');
+  require('dotenv').config();
+
+  const app = express();
+
+  app.use(logger("dev"));
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/recipes", {
+      useNewUrlParser: true,
+      useFindAndModify: false
+  });
+
+  app.use(express.static("client/build"));
+
+  //   if (process.env.NODE_ENV === "production") {
+  //     app.use(express.static("client/build"));
+  //   }
+
+  //app.use(routes);
+
+  app.listen(process.env.PORT || 3000, () => console.log('Now listening'));
