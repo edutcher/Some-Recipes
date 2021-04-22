dd8eaf7cff272ecad5eedfe6e49af953c6d51819
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-04-22 02:04:00 -0400

    shell

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import RecipeCard from "./components/RecipeCard";
+import FlexContainer from "./components/FlexContainer";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { sizing } from '@material-ui/system'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+        <Container style={{display:"flex", flexWrap:"wrap"}}>
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+
+        </Container>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
   const path = require('path');
   const express = require("express");
   const logger = require("morgan");
+//  const MongoStore = require('connect-mongo')(express);
   const mongoose = require("mongoose");
-  //const routes = require('./routes');
+  const routes = require('./routes/htmlRoutes.js');
   require('dotenv').config();
 
   const app = express();
 
   app.use(logger("dev"));
 
+ // app.use(express.cookieParser());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use(express.static(path.join(__dirname, 'public')));
 
   mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/recipes", {
       useNewUrlParser: true,
       useFindAndModify: false
   });
+
+//   app.use(express.session({
+//       secret: conf.secret,
+//       maxAge: new Date(Date.now() + 3600000),
+//       store: new MongoStore(conf.db)
+//   }));
 
   app.use(express.static("client/build"));
 
@@ -24,6 +31,6 @@
   //     app.use(express.static("client/build"));
   //   }
 
-  //app.use(routes);
+  app.use(routes);
 
   app.listen(process.env.PORT || 3000, () => console.log('Now listening'));
